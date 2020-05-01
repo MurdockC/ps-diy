2f94ada0a0103882c7d4db827ebe4bdee1234253
Caleb Murdock <calebdmurdock@gmail.com>
2020-05-01 14:17:11 -0500

    Add files via upload
    Added Product Spec List

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,27 @@
 //openess functions - reduce down to case or if statement
 function openess10() {
   document.getElementById('shade').style.opacity= '.7';
+  document.getElementById('opac').innerHTML='10%';
 }
 
 function openess5() {
   document.getElementById('shade').style.opacity= '.78';
+  document.getElementById('opac').innerHTML='5%';
 }
 
 function openess3() {
   document.getElementById('shade').style.opacity= '.85';
+  document.getElementById('opac').innerHTML='3%';
 }
 
 function openess1() {
   document.getElementById('shade').style.opacity= '.9';
+  document.getElementById('opac').innerHTML='1%';
 }
 
 function openess0() {
   document.getElementById('shade').style.opacity= '1';
+  document.getElementById('opac').innerHTML='0%';
 }
 
 
@@ -24,6 +29,7 @@ function openess0() {
 const overlay = document.getElementById("shade-color-shape");
 const shaderolloverlay = document.getElementById("roll-color-shape");
 const cassetteoverlay = document.getElementById("cassette-color-shape");
+const specfColor = document.getElementById("fcolor");
 
 // Click on a color
 
@@ -35,16 +41,19 @@ for (var i = 0; i < el.length; i++) {
 function changeColor(e) {
   // get the hex color
   let hex = e.target.getAttribute("data-hex");
+  let fcolor = e.target.getAttribute("value");
   // set the hex color
   overlay.style.fill = hex;
   shaderolloverlay.style.fill = hex;
   cassetteoverlay.style.fill = hex;
+  specfColor.innerHTML = fcolor ;
 }
 
 
 // Reference the color shape that was drawn over the image
 const motoroverlay = document.getElementById("motor-color-shape");
 const motoroverlaytwo = document.getElementById("motor-color-shapetwo");
+
 
 // Click on a color
 
@@ -64,8 +73,9 @@ function changeMotorColor(e) {
 
 // Reference the color shape that was drawn over the image
 const rolloverlay = document.getElementById("hardware-color-shape");
-const hardwareone = document.getElementById("motor-color-shape");
-const hardwaretwo = document.getElementById("motor-color-shapetwo");
+const rollhardwareone = document.getElementById("roll-hardware-shape-one");
+const rollhardwaretwo = document.getElementById("roll-hardware-shape-two");
+const spechColor = document.getElementById("hcolor");
 
 // Click on a color
 
@@ -77,13 +87,13 @@ for (var i = 0; i < mot.length; i++) {
 function changeRollColor(e) {
   // get the hex color
   let hexcolor = e.target.getAttribute("data-hex");
+  let hcolor = e.target.getAttribute("value");
   // set the hex color
   rolloverlay.style.fill = hexcolor;
-  hardwareone.style.fill = hexcolor;
-  hardwaretwo.style.fill = hexcolor;
+  rollhardwareone.style.fill = hexcolor;
+  rollhardwaretwo.style.fill = hexcolor;
+  spechColor.innerHTML = hcolor;
 }
-
-
 
 // show/hide side view for hardware
 
@@ -100,8 +110,10 @@ function toggleImagesoff() {
 function styleSwap() {
   document.getElementsByClassName("cassette")[0].style.display = "block";
   document.getElementsByClassName("cassette")[1].style.display = "block";
+  document.getElementById('hstyle').innerHTML='Cassette';
 }
 function styleSwapback() {
   document.getElementsByClassName("cassette")[0].style.display = "none";
   document.getElementsByClassName("cassette")[1].style.display = "none";
+  document.getElementById('hstyle').innerHTML='Open Roll';
 }
